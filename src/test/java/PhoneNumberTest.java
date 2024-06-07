c9ed5d67d56cb66f2f9ac4b14b9ba401046312b2
marmiecode <mieczkowska.maartyna@gmail.com>
2024-06-07 03:07:28 +0200

    Started phone number contact tests Added initial code to start testing phone number contacts

--- src/test/java/PhoneNumberTest.java
+++ src/test/java/PhoneNumberTest.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HalloweenPartyTest {
+public class PhoneNumberTest {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://candymapper.com/");
