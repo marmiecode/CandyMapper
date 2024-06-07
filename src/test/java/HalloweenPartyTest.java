5338b1e8455b403660347f5cc3d469863c3ba547
marmiecode <mieczkowska.maartyna@gmail.com>
2024-06-07 02:29:08 +0200

    Added automated tests for Halloween Party page

---
+++ src/test/java/HalloweenPartyTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HalloweenPartyTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://candymapper.com/");
+
+        WebElement xButton = driver.findElement(By.id("popup-widget111379-close-icon"));
+        xButton.click();
+
+        WebElement halloweenPartyLink = driver.findElement(By.xpath("//a[contains(text(), 'Halloween Party')]"));
+        halloweenPartyLink.click();
+
+        WebElement hostingLink = driver.findElement(By.xpath("//a[contains(text(), 'I Am Hosting A Party')]"));
+        hostingLink.click();
+
+        WebElement zombiesButton = driver.findElement(By.xpath("//a[contains(text(), 'Zombies')]"));
+        zombiesButton.click();
+
+
+    }
+}
